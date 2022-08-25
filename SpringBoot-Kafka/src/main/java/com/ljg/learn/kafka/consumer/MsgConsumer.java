25d08b466ef88a1984f159e6820fb3fd855d2149
jiagenli <jiagenli@foxmail.com>
2022-08-25 15:10:58 +0800

    [feature]kafka，测试使用ProducerRecord和ConsumerRecord

--- SpringBoot-Kafka/src/main/java/com/ljg/learn/kafka/consumer/MsgConsumer.java
+++ SpringBoot-Kafka/src/main/java/com/ljg/learn/kafka/consumer/MsgConsumer.java
@@ -1,6 +1,7 @@
 package com.ljg.learn.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,8 +11,19 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics = {"mingyue"}, groupId = "test-consumer-group")
 public class MsgConsumer {
 
-    @KafkaHandler
-    public void receive(String message) {
-        log.info("消费者收到的消息是：" + message);
+//    @KafkaHandler
+//    public void receive(String message) {
+//        log.info("消费者收到的消息是：" + message);
+//    }
+
+    /**
+     * 这个方法是测试用ConsumerRecord来接收消息
+     * 一开始的时候报了这个异常 No method found for class java.lang.String
+     * 原因是因为没有加 (isDefault = true)
+     * @param consumerRecord
+     */
+    @KafkaHandler(isDefault = true)
+    public void receiveRecord(ConsumerRecord<String, String> consumerRecord) {
+        log.info("测试用consumerRecord得到的消息：" + consumerRecord.key() + consumerRecord.value());
     }
 }
--- SpringBoot-Kafka/src/main/java/com/ljg/learn/kafka/controller/MsgProducerController.java
+++ SpringBoot-Kafka/src/main/java/com/ljg/learn/kafka/controller/MsgProducerController.java
@@ -1,5 +1,6 @@
 package com.ljg.learn.kafka.controller;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,16 @@ public class MsgProducerController {
     @RequestMapping("/send")
     public void send(String message) {
         kafkaTemplate.send("mingyue", message);
+    }
+
+    /**
+     * 其实这个方法是来测试直接发ProducerRecord会怎么样
+     * 点进send方法一看，其实不管是String还是其他对象，send方法内部都是发送的ProducerRecord
+     * @param message
+     */
+    @RequestMapping("/send2")
+    public void send2(String message) {
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("mingyue", message);
+        kafkaTemplate.send(producerRecord);
     }
 }
