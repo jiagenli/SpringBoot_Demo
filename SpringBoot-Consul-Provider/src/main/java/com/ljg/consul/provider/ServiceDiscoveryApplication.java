05ece175a2b16b2038f977dd039f76933838a450
jiagenli <jiagenli@foxmail.com>
2022-09-14 16:00:41 +0800

    initialize consul

--- SpringBoot-Consul-Provider/src/main/java/com/ljg/consul/provider/ServiceDiscoveryApplication.java
+++ SpringBoot-Consul-Provider/src/main/java/com/ljg/consul/provider/ServiceDiscoveryApplication.java
@@ -2,14 +2,12 @@ package com.ljg.consul.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class ServiceDiscoveryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
     }
 }
