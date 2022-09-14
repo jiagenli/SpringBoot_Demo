11bc8582a1e3100fba186c83d8fc151cd12dbecc
jiagenli <jiagenli@foxmail.com>
2022-09-14 16:15:28 +0800

    add feature my custom check

--- SpringBoot-Consul-Provider/src/main/java/com/ljg/consul/provider/controller/ProviderController.java
+++ SpringBoot-Consul-Provider/src/main/java/com/ljg/consul/provider/controller/ProviderController.java
@@ -1,6 +1,7 @@
 package com.ljg.consul.provider.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,5 +14,12 @@ public class ProviderController {
     @GetMapping("/provider")
     public void query() {
         log.info("远程调用成功" + System.currentTimeMillis());
+    }
+
+    @GetMapping("/my-health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        String message = "Testing my healh check function";
+        log.info(message);
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
     }
 }
