0823e271b1c334f043f5d106f068c07c2a1a7cdd
jiagenli <jiagenli@foxmail.com>
2022-09-16 16:51:34 +0800

    nacos provider

---
+++ SpringBoot-Nacos-Provider/src/main/java/com/ljg/learn/nacos/controller/ProviderController.java
@@ -0,0 +1,24 @@
+package com.ljg.learn.nacos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ProviderController {
+
+    @GetMapping("/provider")
+    public void query() {
+        log.info("远程调用成功" + System.currentTimeMillis());
+    }
+
+    @GetMapping("/my-health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        String message = "Testing my healh check function";
+        log.info(message);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
