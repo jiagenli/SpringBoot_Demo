82a859f5269bad77c222aa727f772c082411adec
jiagenli <jiagenli@foxmail.com>
2022-11-05 23:41:39 +0800

    spring cloud openfeign

---
+++ SpringBoot-Openfeign/src/main/java/com/ljg/learn/openfeign/OpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.ljg.learn.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OpenfeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignApplication.class);
+    }
+}
